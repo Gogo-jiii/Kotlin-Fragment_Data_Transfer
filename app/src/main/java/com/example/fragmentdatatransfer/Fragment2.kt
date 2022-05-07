c9b439e26cf1173bf6435e585c3ee4df6adb4c0b
VgionTech <(0$t\U9i9b.{3k*>
2022-05-07 21:21:18 +0530

    init

---
+++ app/src/main/java/com/example/fragmentdatatransfer/Fragment2.kt
@@ -0,0 +1,20 @@
+package com.example.fragmentdatatransfer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class Fragment2 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_2, container, false)
+        val name = requireArguments().getString("name")
+        Toast.makeText(activity, name, Toast.LENGTH_SHORT).show()
+        return view
+    }
+}
